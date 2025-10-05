c3ad66afeabb3c7415814d7119c2ff39e90c3e2c
DastRay <paha191238@gmail.com>
2025-10-05 20:20:58 +0300

    Изменения в движке бота

--- app/database/dp.py
+++ app/database/dp.py
@@ -11,8 +11,7 @@
 - SQLAlchemy ORM для работы с моделями и запросами.
 """
 
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from app.config import settings
 from sqlalchemy.pool import AsyncAdaptedQueuePool
 
@@ -20,28 +19,12 @@ engine = create_async_engine(
     settings.DB_TIMETABLE_URL,        # строка подключения к PostgreSQL
     echo=False,                       # echo=True = логировать SQL-запросы в консоль
     future=True,                      # новый API SQLAlchemy 2.0
-    poolclass=AsyncAdaptedQueuePool,  # включаем пул соединений
+    poolclass=AsyncAdaptedQueuePool,  # пул соединений
     pool_size=10,                     # держим до 10 соединений открытыми
     max_overflow=20                   # создаётся ещё 20 соединений при пике нагрузки
 )
 
-AsyncSessionLocal = sessionmaker(
+AsyncSessionLocal = async_sessionmaker(
     bind=engine,
-    class_=AsyncSession,
     expire_on_commit=False
 )
-
-async def get_session() -> AsyncSession:
-    """
-    Возвращает новую асинхронную сессию для работы с базой данных.
-
-    Пример использования:
-        async with get_session() as session:
-            await session.execute(...)
-            await session.commit()
-
-    Возвращает:
-        AsyncSession: объект асинхронной сессии SQLAlchemy
-    """
-
-    return AsyncSessionLocal()
