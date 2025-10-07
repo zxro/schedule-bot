e61283136a9e5d81de8b33b63c6ab42d07aac3e5
DastRay <paha191238@gmail.com>
2025-10-07 22:45:56 +0300

    Изменение в формате вывода расписания

--- app/keyboards/sync_kb.py
+++ app/keyboards/sync_kb.py
@@ -39,6 +39,7 @@ def get_type_sync_kb():
             [InlineKeyboardButton(text="Синхронизация расписания для всех", callback_data="sync_university")],
             [InlineKeyboardButton(text="Синхронизация расписания для факультета", callback_data="sync_faculty")],
             [InlineKeyboardButton(text="Синхронизация расписания для группы", callback_data="sync_group")],
+            [InlineKeyboardButton(text="❌ Отмена синхронизации", callback_data="cancel_choice_sync")]
         ]
     )
 
