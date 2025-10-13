33272b0cd0c36be3aa298c3bbd90c98857a7195d
DastRay <paha191238@gmail.com>
2025-10-13 22:16:07 +0300

    Модернизация логирования

--- app/utils/custom_logging/setup.py
+++ app/utils/custom_logging/setup.py
@@ -9,6 +9,7 @@
 import logging
 from aiogram import Bot
 from app.config import settings
+from app.utils.custom_logging.ContextFilter import ContextFilter
 from app.utils.custom_logging.TelegramLogHandler import TelegramLogHandler
 
 def setup_logging(bot: Bot) -> logging.Logger:
@@ -29,15 +30,25 @@ def setup_logging(bot: Bot) -> logging.Logger:
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
 
+    context_filter = ContextFilter()
+
+    log_format = (
+        "%(asctime)s [%(levelname)s] "
+        "[u_id=%(user_id)s u_n=@%(username)s] "
+        "%(name)s: %(message)s"
+    )
+
     # --- консоль ---
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
-    console_handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s"))
+    console_handler.setFormatter(logging.Formatter(log_format))
+    console_handler.addFilter(context_filter)
     logger.addHandler(console_handler)
 
     # --- телеграм ---
     tg_handler = TelegramLogHandler(bot, settings.TELEGRAM_LOG_CHAT_ID, level=logging.WARNING)
-    tg_handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s"))
+    tg_handler.setFormatter(logging.Formatter(log_format))
+    tg_handler.addFilter(context_filter)
     logger.addHandler(tg_handler)
 
     return logger
